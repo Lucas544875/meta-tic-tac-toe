2709181141f6c84a66234c2b6718277d8b64caec
Lucas544875 <takayanagi691@gmail.com>
2024-08-15 15:49:17 +0900

    [delete] ロードシーンの削除

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,6 +1,6 @@
-import { LoadingScene } from './LoadingScene';
 import { TitleScene } from './TitleScene';
+import { SelectDifficultyScene } from './SelectDifficultyScene';
 import { MainScene } from './MainScene';
 import { EndingScene } from './EndingScene';
 
-export const Scenes = [LoadingScene, TitleScene, MainScene, EndingScene];
+export const Scenes = [TitleScene, SelectDifficultyScene, MainScene, EndingScene];

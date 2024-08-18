ccb7e21642ac5ae256db067e483c7b9cac6645c4
Lucas544875 <takayanagi691@gmail.com>
2024-08-18 15:17:54 +0900

    [fix] 関数の書き方を改善

--- src/scene/SelectDifficultyScene.ts
+++ src/scene/SelectDifficultyScene.ts
@@ -2,31 +2,28 @@ export class SelectDifficultyScene extends Phaser.Scene {
   constructor() {
     super("selectDifficulty");
   }
+
   preload() {
     this.load.image('easy', 'assets/solo.png');
     this.load.image('hard', 'assets/solo.png');
     this.load.image('veryhard', 'assets/solo.png');
   }
 
-  create() {
+  private addMode(x:number, y:number, mode:"easy"|"hard"|"veryhard") {
     const { width, height } = this.game.canvas;
+    return this.add.image(width/2 + x, height/2 + y, mode)
+    .setDisplaySize(300, 300)
+    .setInteractive({
+      useHandCursor: true
+    })
+    .on('pointerdown', () => {
+      this.scene.start('main', { gameMode: 'solo' , difficulty: mode});
+    });
+  }
 
-    function addMode(scene:Phaser.Scene,x:integer, y:integer,  mode:"easy"|"hard"|"veryhard") {
-      return scene.add.image(width/2 + x, height/2 + y, mode)
-      .setDisplaySize(300, 300)
-      .setInteractive({
-        useHandCursor: true
-      })
-      .on('pointerdown', () => {
-        scene.scene.start('main', { gameMode: 'solo' , difficulty: mode});
-      });
-    }
-    
-    const easyMode = addMode(this, 0, 200, "easy");
-
-    const hardMode = addMode(this, 0, 0, "hard");
-
-    const veryhardMode = addMode(this, 0, -200, "veryhard");
-
+  create() {
+    this.addMode(0, 200, "easy");
+    this.addMode(0, 0, "hard");
+    this.addMode(0, -200, "veryhard");
   }
 }

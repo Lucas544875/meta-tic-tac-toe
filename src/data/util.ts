e7682a7e850336b5962361889d682b38a885f865
Lucas544875 <takayanagi691@gmail.com>
2024-08-19 15:28:15 +0900

    [delete] 不要になったファイルを削除

--- src/data/util.ts
+++
@@ -1,7 +0,0 @@
-export function copyBoadState(boardState: ("0"|"1"|"-") [][][][]): ("0"|"1"|"-") [][][][] {
-  return boardState.map((metaRow) => metaRow.map((metaCell) => metaCell.map((subRow) => [...subRow])));
-}
-
-export function copyMetaBoadState(metaBoardState: ("0"|"1"|"-")[][]): ("0"|"1"|"-")[][] {
-  return metaBoardState.map((row) => [...row]);
-}

eb527948e683a49f7258d7361815a85e3a36d337
Lucas544875 <takayanagi691@gmail.com>
2024-08-20 21:01:15 +0900

    [update] easyのロジックを実装

--- src/class/agent.ts
+++ src/class/agent.ts
@@ -11,13 +11,13 @@ export class Agent {
     this.difficulty = difficulty;
     switch (difficulty){
       case "veryhard":
-        this.play = this.randomStrategy;
+        this.play = this.veryhardStrategy;
         break;
       case "hard":
-        this.play = this.randomStrategy;
+        this.play = this.hardStrategy;
         break;
       case "easy":
-        this.play = this.randomStrategy;
+        this.play = this.easyStrategy;
     }
   }
 
@@ -33,5 +33,42 @@ export class Agent {
     const availableCells = BoadManager.availableCells(this.gameState);
     const randomIndex = Math.floor(Math.random() * availableCells.length);
     return availableCells[randomIndex];
+  }
+
+  private easyStrategy(gameState: GameState): {i:number, j:number, k:number, l:number} {
+    const availableCells = BoadManager.availableCells(gameState);
+    let candidate = []
+    // 揃える手があればその中からランダム
+    for (const cell of availableCells) {
+      let metaCell = BoadManager.copyMetaBoadState(gameState.boadState[cell.i][cell.j]);
+      metaCell[cell.k][cell.l] = "1";      
+      if (BoadManager.checkWinner(metaCell) === "1") {
+        candidate.push(cell);
+      }
+    }
+    if (candidate.length > 0) {
+      return candidate[Math.floor(Math.random() * candidate.length)];
+    }
+    // リーチを防ぐ手があればその中からランダム
+    for (const cell of availableCells) {
+      let metaCell = BoadManager.copyMetaBoadState(gameState.boadState[cell.i][cell.j]);
+      metaCell[cell.k][cell.l] = "0";      
+      if (BoadManager.checkWinner(metaCell) === "0") {
+        candidate.push(cell);
+      }
+    }
+    if (candidate.length > 0) {
+      return candidate[Math.floor(Math.random() * candidate.length)];
+    }
+    // それ以外はランダム
+    return availableCells[Math.floor(Math.random() * availableCells.length)];
+  }
+
+  private hardStrategy(gameState: GameState): {i:number, j:number, k:number, l:number} {
+    return this.randomStrategy(gameState);
+  }
+
+  private veryhardStrategy(gameState: GameState): {i:number, j:number, k:number, l:number} {
+    return this.randomStrategy(gameState);
   }
 }

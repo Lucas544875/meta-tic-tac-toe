760fa801c1dbdad451fb6ffef9fb0565b2b6ad96
Lucas544875 <takayanagi691@gmail.com>
2024-08-28 11:36:08 +0900

    [update] 難易度選択の素材を変更

--- src/scene/SelectDifficultyScene.ts
+++ src/scene/SelectDifficultyScene.ts
@@ -5,12 +5,12 @@ export class SelectDifficultyScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('easy', 'assets/solo.png');
-    this.load.image('hard', 'assets/solo.png');
-    this.load.image('veryhard', 'assets/solo.png');
+    this.load.image('easy', 'assets/easy.png');
+    this.load.image('hard', 'assets/hard.png');
+    this.load.image('veryhard', 'assets/veryhard.png');
   }
 
-  private addMode(x:number, y:number, mode:"easy"|"hard"|"veryhard") {
+  private addMode(x:number, y:number, imgWidth:number, imgHeight:number,  mode:"easy"|"hard"|"veryhard") {
     const { width, height } = this.game.canvas;
     const gameState:GameState = {
       player: "0",
@@ -20,7 +20,7 @@ export class SelectDifficultyScene extends Phaser.Scene {
       metaBoadState: Array(3).fill(Array(3).fill("-"))
     };
     return this.add.image(width/2 + x, height/2 + y, mode)
-    .setDisplaySize(300, 300)
+    .setDisplaySize(imgWidth, imgHeight)
     .setInteractive({
       useHandCursor: true
     })
@@ -33,14 +33,14 @@ export class SelectDifficultyScene extends Phaser.Scene {
     const { width, height } = this.game.canvas;
     const background = this.add.image(width/2, height/2, 'background').setDisplaySize(width, height);
 
-    this.addMode(0, 200, "easy");
-    this.add.text(this.game.canvas.width/2-25, this.game.canvas.height/2 + 190, 'easy', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
+    this.addMode(0, 170, 450*0.8, 200*0.8, "easy");
+    this.add.text(this.game.canvas.width/2, this.game.canvas.height/2 + 177, 'EASY', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
 
-    this.addMode(0, 0, "hard");
-    this.add.text(this.game.canvas.width/2-25, this.game.canvas.height/2 , 'hard', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
+    this.addMode(0, 0, 512*0.8, 200*0.8, "hard");
+    this.add.text(this.game.canvas.width/2, this.game.canvas.height/2+4 , 'HARD', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
 
-    this.addMode(0, -200, "veryhard");
-    this.add.text(this.game.canvas.width/2-25, this.game.canvas.height/2 - 200, 'veryhard', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
+    this.addMode(0, -170, 512*0.8, 210*0.8, "veryhard");
+    this.add.text(this.game.canvas.width/2, this.game.canvas.height/2 - 163, 'VERYHARD', {fontSize: 30, fontFamily: "meiryo UI", color: "#000"}).setOrigin(0.5);
 
   }
 }

531a275579b6f0f4663ddae9c585835d73cb34cc
Lucas544875 <takayanagi691@gmail.com>
2024-09-23 22:59:33 +0900

    [add] ローディング画面の追加

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,6 +1,7 @@
+import { LoadingScene } from './LoadingScene';
 import { TitleScene } from './TitleScene';
 import { SelectDifficultyScene } from './SelectDifficultyScene';
 import { MainScene } from './MainScene';
 import { EndingScene } from './EndingScene';
 
-export const Scenes = [TitleScene, SelectDifficultyScene, MainScene, EndingScene];
+export const Scenes = [LoadingScene, TitleScene, SelectDifficultyScene, MainScene, EndingScene];
